7b43dc6be89fafe4ad2add92a1ab52ad6d822404
martakus <markus@Jarvis.local>
2014-09-25 20:44:51 -0600

    clean non-pages files

--- reconstructMRI.m
+++
@@ -1,4 +0,0 @@
-function image = reconstructMRI (raw_image)
-	% image = reconstructMRI (raw_image) 
-	% returns the complex image in the same number of dimensions as the raw_image variable of the k-space via an inverse fourier transform
-	image = fftshift(ifftn(raw_image));
